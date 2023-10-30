a6f79eca00ebec3661b911129ce8e0a0d3cf1adb
S4MUEL505 <142861548+S4MUEL505@users.noreply.github.com>
2023-10-30 09:03:31 -0600

    Se inicia la creación de la Galería de Imágenes
    Se cambia el nombre de vue.vue a App.vue, se le añade algunas imágenes y sus títulos.

---
+++ App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <h1>Galería de imágenes con Vue</h1>
+    <div class="gallery">
+      <div v-for="image in images" :key="image.id" class="item">
+        <img :src="image.url" :alt="image.title">
+        <p>{{ image.title }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      images: [
+        { id: 1, url: "https://as01.epimg.net/diarioas/imagenes/2022/05/29/actualidad/1653826510_995351_1653826595_noticia_normal_recorte1.jpg", title: "Perro" },
+        { id: 2, url: "https://s1.ppllstatics.com/lasprovincias/www/multimedia/202112/12/media/cortadas/gatos-kb2-U160232243326NVC-1248x770@Las%20Provincias.jpg", title: "Gato" },
+        { id: 3, url: "https://www.arteescuela.com/wp-content/uploads/2022/10/como-dibujar-un-pajaro-paso-a-paso.jpg", title: "Pájaro" },
+        { id: 4, url: "https://concepto.de/wp-content/uploads/2018/08/Londres-e1533855310803.jpg", title: "Ciudad" },
+        { id: 5, url: "https://concepto.de/wp-content/uploads/2018/08/monta%C3%B1a-clima-min-e1533762913759.jpg", title: "Montaña" }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+.gallery {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 10px;
+}
+
+.item {
+  text-align: center;
+}
+
+img {
+  width: 100%;
+}
+</style>
